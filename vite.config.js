8ea677a5650171cd3f70d86a3a25971ecaa8619e
AlannaYL <a04810254@gmail.com>
2023-10-24 12:06:30 +0800

    [upload]新增vite跟ejs環境

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
+import WindiCSS from 'vite-plugin-windicss'
+import liveReload from 'vite-plugin-live-reload'
+
+export default defineConfig({
+  server: {
+    open: '/src/pages/front/index.html' // 指定打開路徑
+  },
+  plugins: [
+    liveReload(['./public/layouts/*.ejs', '.src/pages/**/*.ejs', '.src/pages/**/*.html']),
+    ViteEjsPlugin(),
+    WindiCSS()
+  ]
+})
