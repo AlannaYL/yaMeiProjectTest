28d8acaa3fdf1ed7d9bb4b5805a6c72034a44977
Alanna <113668483+AlannaYL@users.noreply.github.com>
2023-10-24 17:21:51 +0800

    Revert "Develop"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // layouts 的ejs 也可顯示樣式
-  content: ['./public/layouts/*.ejs', './src/**/*.{html,js}', './dist/**/*.html'],
+  content: ['./src/**/*.{html,js}', './dist/**/*.html'],
   theme: {
     extend: {}
   },
