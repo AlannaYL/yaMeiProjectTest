84752633883b1b98f7898bb23fdd407dc052b317
AlannaYL <a04810254@gmail.com>
2023-10-24 12:50:00 +0800

    [upload]調整ejs路徑測試

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import WindiCSS from 'vite-plugin-windicss'
 import liveReload from 'vite-plugin-live-reload'
 
 export default defineConfig({
+  // base: '/yaMeiProject/',
   server: {
     open: '/src/pages/front/index.html' // 指定打開路徑
   },
   plugins: [
-    liveReload(['./public/layouts/*.ejs', '.src/pages/**/*.ejs', '.src/pages/**/*.html']),
+    liveReload(['.public/layouts/*.ejs', '.src/pages/**/*.ejs', '.src/pages/**/*.html']),
     ViteEjsPlugin(),
     WindiCSS()
   ]
